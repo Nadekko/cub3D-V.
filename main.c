632abae3b56dee9d030e1a5ac64a2bdb73e6a8db
Nadekko <anyssa.djennad@gmail.com>
2025-02-17 20:17:28 +0100

    test img

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
+/*   By: andjenna <andjenna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 21:41:31 by andjenna          #+#    #+#             */
-/*   Updated: 2025/02/17 13:20:29 by ede-cola         ###   ########.fr       */
+/*   Updated: 2025/02/17 19:19:50 by andjenna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,65 @@ static void	ft_display_game(t_data *data)
 	mlx_loop_hook(data->mlx->mlx, &ft_play, data);
 	mlx_hook(data->mlx->win, 3, 1L << 1, key_release, data);
 	mlx_loop(data->mlx->mlx);
+}
+
+int	ft_init_matrix(t_data *data)
+{
+	data->map->width = ft_strlen(data->map->map_tab[0]);
+	data->map->height = ft_tab_len(data->map->map_tab);
+	data->map->map_int = ft_convert_map(data->map->map_tab);
+	if (mlx_window_init(data))
+		return (ft_free_data(data),
+			ft_putendl_fd("Error initalizing window failed", 2), 1);
+	ft_clean_init_player(data);
+	ft_clean_init_raycast(data);
+	ft_clean_init_move(data);
+	if (ft_get_player_pos(data))
+		return (ft_free_data(data), ft_putendl_fd("Error no player position",
+				2), 1);
+	ft_display_game(data);
+	ft_free_data(data);
+	return (0);
+}
+
+int	ft_init_game(t_data *data, char *map_file)
+{
+	char	**file;
+
+	file = ft_read_map(map_file);
+	if (!file)
+		return (ft_putendl_fd("Error can't read map", 2), 1);
+	ft_clean_init_data(data);
+	if (ft_get_data(data, file))
+	{
+		if (ft_data_start(data, file))
+			return (1);
+	}
+	else
+		return (ft_free_data(data), ft_free_tab(file),
+			ft_putendl_fd("Error invalid file", 2), 1);
+	ft_free_tab(file);
+	if (ft_init_matrix(data))
+		return (1);
+	return (0);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	t_data	data;
+
+	if (!env || !*env)
+		return (ft_putendl_fd("Error env is needed to launch cub3d", 2), 1);
+	else if (ac != 2)
+		return (ft_putendl_fd("Error wrong arguments count", 2), 1);
+	else if (!ft_check_map_extension(av[1]))
+		return (ft_putendl_fd("Error invalid map extension", 2), 1);
+	else
+	{
+		if (ft_init_game(&data, av[1]))
+			return (1);
+	}
+	return (0);
 }
 
 // int	ft_init_game(t_data *data, char *map_file)
@@ -108,64 +167,3 @@ static void	ft_display_game(t_data *data)
 // 		ft_free_data(data);
 // 		return (0);
 // }
-
-int	ft_init_matrix(t_data *data)
-{
-	data->map->width = ft_strlen(data->map->map_tab[0]);
-	data->map->height = ft_tab_len(data->map->map_tab);
-	data->map->map_int = ft_convert_map(data->map->map_tab);
-	if (mlx_window_init(data))
-		return (ft_free_data(data),
-			ft_putendl_fd("Error initalizing window failed", 2), 1);
-	ft_clean_init_player(data);
-	ft_clean_init_raycast(data);
-	ft_clean_init_move(data);
-	if (ft_get_player_pos(data))
-		return (ft_free_data(data), ft_putendl_fd("Error no player position",
-				2), 1);
-	ft_display_game(data);
-	ft_free_data(data);
-	return (0);
-}
-
-int	ft_init_game(t_data *data, char *map_file)
-{
-	char	**file;
-	size_t	i;
-
-	file = ft_read_map(map_file);
-	if (!file)
-		return (ft_putendl_fd("Error can't read map", 2), 1);
-	i = 0;
-	ft_clean_init_data(data);
-	if (ft_get_data(data, file))
-	{
-		if (ft_data_start(data, file))
-			return (1);
-	}
-	else
-		return (ft_free_data(data), ft_free_tab(file),
-			ft_putendl_fd("Error invalid file", 2), 1);
-	ft_free_tab(file);
-	if (ft_init_matrix(data))
-		return (1);
-	return (0);
-}
-
-int	main(int ac, char **av, char **env)
-{
-	t_data	data;
-
-	if (!env || !*env)
-		return (ft_putendl_fd("Error env is needed to launch cub3d", 2), 1);
-	else if (ac != 2)
-		return (ft_putendl_fd("Error wrong arguments count", 2), 1);
-	else if (!ft_check_map_extension(av[1]))
-		return (ft_putendl_fd("Error invalid map extension", 2), 1);
-	else
-	{
-		if (ft_init_game(&data, av[1]))
-			return (1);
-	}
-	return (0);
-}
